f671db2c062b35d47d1b1a9e971fe9abc090b98a
Tan Yanbo <tanyanbo95@hotmail.com>
2023-01-21 14:31:33 +0800

    Refactor 2 test files to use custom matcher

--- __tests__/array-literal.test.ts
+++ __tests__/array-literal.test.ts
@@ -1,99 +1,84 @@
-import { Parser } from "../src/parser";
-import { astFactory } from "../src/helpers/ast-factory";
-
 describe("array literal", () => {
   it("should parse an array literal with no elements correctly", () => {
-    const parser = new Parser(`
+    expect(`
       const x = []
-    `);
-    const ast = parser.parse();
-    expect(ast).toEqual(
-      astFactory([
-        {
-          type: "VariableDeclaration",
-          id: {
-            type: "Identifier",
-            name: "x",
-          },
-          value: {
-            type: "ArrayLiteral",
-            elements: [],
-          },
-          kind: "const",
+    `).toHaveAst([
+      {
+        type: "VariableDeclaration",
+        id: {
+          type: "Identifier",
+          name: "x",
         },
-      ])
-    );
+        value: {
+          type: "ArrayLiteral",
+          elements: [],
+        },
+        kind: "const",
+      },
+    ]);
   });
 
   it("should parse an array literal with one element correctly", () => {
-    const parser = new Parser(`
+    expect(`
       const x = [1]
-    `);
-    const ast = parser.parse();
-    expect(ast).toEqual(
-      astFactory([
-        {
-          type: "VariableDeclaration",
-          id: {
-            type: "Identifier",
-            name: "x",
-          },
-          value: {
-            type: "ArrayLiteral",
-            elements: [
-              {
-                type: "NumericLiteral",
-                value: 1,
-              },
-            ],
-          },
-          kind: "const",
+    `).toHaveAst([
+      {
+        type: "VariableDeclaration",
+        id: {
+          type: "Identifier",
+          name: "x",
         },
-      ])
-    );
+        value: {
+          type: "ArrayLiteral",
+          elements: [
+            {
+              type: "NumericLiteral",
+              value: 1,
+            },
+          ],
+        },
+        kind: "const",
+      },
+    ]);
   });
 
   it("should parse an array literal with multiple elements correctly", () => {
-    const parser = new Parser(`
+    expect(`
       const x = [1, test() + 3]
-    `);
-    const ast = parser.parse();
-    expect(ast).toEqual(
-      astFactory([
-        {
-          type: "VariableDeclaration",
-          id: {
-            type: "Identifier",
-            name: "x",
-          },
-          value: {
-            type: "ArrayLiteral",
-            elements: [
-              {
-                type: "NumericLiteral",
-                value: 1,
-              },
-              {
-                type: "BinaryExpression",
-                left: {
-                  type: "CallExpression",
-                  callee: {
-                    type: "Identifier",
-                    name: "test",
-                  },
-                  arguments: [],
-                },
-                operator: "+",
-                right: {
-                  type: "NumericLiteral",
-                  value: 3,
-                },
-              },
-            ],
-          },
-          kind: "const",
+    `).toHaveAst([
+      {
+        type: "VariableDeclaration",
+        id: {
+          type: "Identifier",
+          name: "x",
         },
-      ])
-    );
+        value: {
+          type: "ArrayLiteral",
+          elements: [
+            {
+              type: "NumericLiteral",
+              value: 1,
+            },
+            {
+              type: "BinaryExpression",
+              left: {
+                type: "CallExpression",
+                callee: {
+                  type: "Identifier",
+                  name: "test",
+                },
+                arguments: [],
+              },
+              operator: "+",
+              right: {
+                type: "NumericLiteral",
+                value: 3,
+              },
+            },
+          ],
+        },
+        kind: "const",
+      },
+    ]);
   });
 });
--- __tests__/binary-expression.test.ts
+++ __tests__/binary-expression.test.ts
@@ -1,138 +1,83 @@
-import { Parser } from "../src/parser";
-import { astFactory } from "../src/helpers/ast-factory";
-
 describe("binary expression", () => {
   it("should parse binary expression with plus operator correctly", () => {
-    const parser = new Parser(`
+    expect(`
       2 + 5
-    `);
-    const ast = parser.parse();
-    expect(ast).toEqual(
-      astFactory([
-        {
-          type: "BinaryExpression",
-          left: {
-            type: "NumericLiteral",
-            value: 2,
-          },
-          operator: "+",
-          right: {
-            type: "NumericLiteral",
-            value: 5,
-          },
+    `).toHaveAst([
+      {
+        type: "BinaryExpression",
+        left: {
+          type: "NumericLiteral",
+          value: 2,
         },
-      ])
-    );
+        operator: "+",
+        right: {
+          type: "NumericLiteral",
+          value: 5,
+        },
+      },
+    ]);
   });
 
   it("should parse binary expression with multiply operator correctly", () => {
-    const parser = new Parser(`
+    expect(`
       2 * 5
-    `);
-    const ast = parser.parse();
-    expect(ast).toEqual(
-      astFactory([
-        {
+    `).toHaveAst([
+      {
+        type: "BinaryExpression",
+        left: {
+          type: "NumericLiteral",
+          value: 2,
+        },
+        operator: "*",
+        right: {
+          type: "NumericLiteral",
+          value: 5,
+        },
+      },
+    ]);
+  });
+
+  it("should parse binary expression with plus and multiply operator correctly", () => {
+    expect(`
+      3 + 2 * 5
+    `).toHaveAst([
+      {
+        type: "BinaryExpression",
+        left: {
+          type: "NumericLiteral",
+          value: 3,
+        },
+        operator: "+",
+        right: {
           type: "BinaryExpression",
+          operator: "*",
           left: {
             type: "NumericLiteral",
             value: 2,
           },
-          operator: "*",
           right: {
             type: "NumericLiteral",
             value: 5,
           },
         },
-      ])
-    );
-  });
-
-  it("should parse binary expression with plus and multiply operator correctly", () => {
-    const parser = new Parser(`
-      3 + 2 * 5
-    `);
-    const ast = parser.parse();
-    expect(ast).toEqual(
-      astFactory([
-        {
-          type: "BinaryExpression",
-          left: {
-            type: "NumericLiteral",
-            value: 3,
-          },
-          operator: "+",
-          right: {
-            type: "BinaryExpression",
-            operator: "*",
-            left: {
-              type: "NumericLiteral",
-              value: 2,
-            },
-            right: {
-              type: "NumericLiteral",
-              value: 5,
-            },
-          },
-        },
-      ])
-    );
+      },
+    ]);
   });
 
   it("should parse binary expression with multiple plus and multiply operators correctly", () => {
-    const parser = new Parser(`
+    expect(`
       3 + 3 * 2 * 5
-    `);
-    const ast = parser.parse();
-    expect(ast).toEqual(
-      astFactory([
-        {
-          type: "BinaryExpression",
-          left: {
-            type: "NumericLiteral",
-            value: 3,
-          },
-          operator: "+",
-          right: {
-            type: "BinaryExpression",
-            left: {
-              type: "BinaryExpression",
-              left: {
-                type: "NumericLiteral",
-                value: 3,
-              },
-              operator: "*",
-              right: {
-                type: "NumericLiteral",
-                value: 2,
-              },
-            },
-            operator: "*",
-            right: {
-              type: "NumericLiteral",
-              value: 5,
-            },
-          },
+    `).toHaveAst([
+      {
+        type: "BinaryExpression",
+        left: {
+          type: "NumericLiteral",
+          value: 3,
         },
-      ])
-    );
-  });
-
-  it("should parse binary expression with plus, multiply and power operators correctly", () => {
-    const parser = new Parser(`
-      3 + 3 * 2 ** 5
-    `);
-    const ast = parser.parse();
-    expect(ast).toEqual(
-      astFactory([
-        {
+        operator: "+",
+        right: {
           type: "BinaryExpression",
           left: {
-            type: "NumericLiteral",
-            value: 3,
-          },
-          operator: "+",
-          right: {
             type: "BinaryExpression",
             left: {
               type: "NumericLiteral",
@@ -140,144 +85,160 @@ describe("binary expression", () => {
             },
             operator: "*",
             right: {
-              type: "BinaryExpression",
-              left: {
-                type: "NumericLiteral",
-                value: 2,
-              },
-              operator: "**",
-              right: {
-                type: "NumericLiteral",
-                value: 5,
-              },
-            },
-          },
-        },
-      ])
-    );
-  });
-
-  it("should parse binary expression with equality operator correctly", () => {
-    const parser = new Parser(`
-      1 + 1 == 2
-    `);
-    const ast = parser.parse();
-    expect(ast).toEqual(
-      astFactory([
-        {
-          type: "BinaryExpression",
-          left: {
-            type: "BinaryExpression",
-            left: {
               type: "NumericLiteral",
-              value: 1,
-            },
-            operator: "+",
-            right: {
-              type: "NumericLiteral",
-              value: 1,
+              value: 2,
             },
           },
-          operator: "==",
-          right: {
-            type: "NumericLiteral",
-            value: 2,
-          },
-        },
-      ])
-    );
-  });
-
-  it("should parse binary expression with strict equality operator correctly", () => {
-    const parser = new Parser(`
-      1 + 1 === 2
-    `);
-    const ast = parser.parse();
-    expect(ast).toEqual(
-      astFactory([
-        {
-          type: "BinaryExpression",
-          left: {
-            type: "BinaryExpression",
-            left: {
-              type: "NumericLiteral",
-              value: 1,
-            },
-            operator: "+",
-            right: {
-              type: "NumericLiteral",
-              value: 1,
-            },
-          },
-          operator: "===",
-          right: {
-            type: "NumericLiteral",
-            value: 2,
-          },
-        },
-      ])
-    );
-  });
-
-  it("should parse binary expression with bitwise or operator correctly", () => {
-    const parser = new Parser(`
-      2 | 5
-    `);
-    const ast = parser.parse();
-    expect(ast).toEqual(
-      astFactory([
-        {
-          type: "BinaryExpression",
-          left: {
-            type: "NumericLiteral",
-            value: 2,
-          },
-          operator: "|",
+          operator: "*",
           right: {
             type: "NumericLiteral",
             value: 5,
           },
         },
-      ])
-    );
+      },
+    ]);
   });
 
-  it("should parse binary expression with bitwise or, xor and and operators correctly", () => {
-    const parser = new Parser(`
-      2 | 5 & 10 ^ 5
-    `);
-    const ast = parser.parse();
-    expect(ast).toEqual(
-      astFactory([
-        {
+  it("should parse binary expression with plus, multiply and power operators correctly", () => {
+    expect(`
+      3 + 3 * 2 ** 5
+    `).toHaveAst([
+      {
+        type: "BinaryExpression",
+        left: {
+          type: "NumericLiteral",
+          value: 3,
+        },
+        operator: "+",
+        right: {
           type: "BinaryExpression",
           left: {
             type: "NumericLiteral",
-            value: 2,
+            value: 3,
           },
-          operator: "|",
+          operator: "*",
           right: {
             type: "BinaryExpression",
             left: {
-              type: "BinaryExpression",
-              left: {
-                type: "NumericLiteral",
-                value: 5,
-              },
-              operator: "&",
-              right: {
-                type: "NumericLiteral",
-                value: 10,
-              },
+              type: "NumericLiteral",
+              value: 2,
             },
-            operator: "^",
+            operator: "**",
             right: {
               type: "NumericLiteral",
               value: 5,
             },
           },
         },
-      ])
-    );
+      },
+    ]);
+  });
+
+  it("should parse binary expression with equality operator correctly", () => {
+    expect(`
+      1 + 1 == 2
+    `).toHaveAst([
+      {
+        type: "BinaryExpression",
+        left: {
+          type: "BinaryExpression",
+          left: {
+            type: "NumericLiteral",
+            value: 1,
+          },
+          operator: "+",
+          right: {
+            type: "NumericLiteral",
+            value: 1,
+          },
+        },
+        operator: "==",
+        right: {
+          type: "NumericLiteral",
+          value: 2,
+        },
+      },
+    ]);
+  });
+
+  it("should parse binary expression with strict equality operator correctly", () => {
+    expect(`
+      1 + 1 === 2
+    `).toHaveAst([
+      {
+        type: "BinaryExpression",
+        left: {
+          type: "BinaryExpression",
+          left: {
+            type: "NumericLiteral",
+            value: 1,
+          },
+          operator: "+",
+          right: {
+            type: "NumericLiteral",
+            value: 1,
+          },
+        },
+        operator: "===",
+        right: {
+          type: "NumericLiteral",
+          value: 2,
+        },
+      },
+    ]);
+  });
+
+  it("should parse binary expression with bitwise or operator correctly", () => {
+    expect(`
+      2 | 5
+    `).toHaveAst([
+      {
+        type: "BinaryExpression",
+        left: {
+          type: "NumericLiteral",
+          value: 2,
+        },
+        operator: "|",
+        right: {
+          type: "NumericLiteral",
+          value: 5,
+        },
+      },
+    ]);
+  });
+
+  it("should parse binary expression with bitwise or, xor and and operators correctly", () => {
+    expect(`
+      2 | 5 & 10 ^ 5
+    `).toHaveAst([
+      {
+        type: "BinaryExpression",
+        left: {
+          type: "NumericLiteral",
+          value: 2,
+        },
+        operator: "|",
+        right: {
+          type: "BinaryExpression",
+          left: {
+            type: "BinaryExpression",
+            left: {
+              type: "NumericLiteral",
+              value: 5,
+            },
+            operator: "&",
+            right: {
+              type: "NumericLiteral",
+              value: 10,
+            },
+          },
+          operator: "^",
+          right: {
+            type: "NumericLiteral",
+            value: 5,
+          },
+        },
+      },
+    ]);
   });
 });
